957da375ba521c18a727975c3d928cff23a5ee37
Brady Barker <brady.d.barker@gmail.com>
2022-09-06 19:14:46 -0400

    removed comments

--- ruby_exercises/basic_ruby_projects/Stock_Picker.rb
+++ ruby_exercises/basic_ruby_projects/Stock_Picker.rb
@@ -1,5 +1,4 @@
 def stock_picker(days)
-  # buy date and sell date
   profit_indexes = Array.new(2,0)
   days.each_with_index do |buy_price, buy_index|
     next_day = buy_index + 1
@@ -14,11 +13,3 @@ def stock_picker(days)
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10])
-
-#First Step: Grab buy date
-#Using buy date go down all numbers that come after buy date
-#Find total profit of each number:
-##Sell Date - Buy Date
-##Save in profit variable
-#Test profit with new profit, if higher replace profit
-#Return the indexes of the Buy and Sell Date
