cd6f2b4263caf0ec94b67bb27b76d0aba8cecd97
Brady Barker <brady.d.barker@gmail.com>
2022-09-08 21:24:16 -0400

    sorts correctly

--- ruby_exercises/basic_ruby_projects/Bubble_Sort.rb
+++ ruby_exercises/basic_ruby_projects/Bubble_Sort.rb
@@ -1,42 +1,27 @@
 def bubble_sort(array)
-  temp_counter = 0
   first_pointer = 0
-  switch_count = 0
+  has_switched = false
   while true do
-    p array
-    temp_counter += 1
-    if (temp_counter > 20) then
-      break
-    end
-    
     second_pointer = first_pointer + 1
     
     if (array[first_pointer] > array[second_pointer]) then
       holder_variable = array[first_pointer]
       array[first_pointer] = array[second_pointer]
       array[second_pointer] = holder_variable
-    else
-      switch_count += 1
+      has_switched = true
     end
+
     first_pointer += 1
-    if (second_pointer == array.length-1 && switch_count > 0) then
+    if (second_pointer == array.length-1 && has_switched) then
       first_pointer = 0
-      switch_count = 0
-    elsif (second_pointer == array.length-1 && switch_count == 0)
+      has_switched = false
+    elsif (second_pointer == array.length-1 && has_switched == false)
       break
     end
   end
+  return array
 end
 
-bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4,3,78,2,0,2])
 
-# Keep pointer of first and second variable
-# Look at first pointer, if first pointer is greater then second pointer:
-## Swap placement of second pointer with first pointer
-# Move first pointer to next index (previously second pointer)
-# Move second pointer to next index
-# If Second Pointer is pointing at last index
-# If a switch as been made
-## Loop through list again
-# Else if no switch has been made
-## Stop looping and return list
+
