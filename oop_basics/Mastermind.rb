8b2daa28873198808f56eb3114c53ab6139f38bd
Brady Barker <brady.d.barker@gmail.com>
2022-09-29 21:38:56 -0400

    Updated how code_maker object is created for computer

--- oop_basics/Mastermind.rb
+++ oop_basics/Mastermind.rb
@@ -8,25 +8,28 @@ class CodeMaker
 end
 
 class CodeBreaker
+  def initialize()
+    @guess_history = []
+  end
 
+  def make_guess()
+    
+  end
 end
-
-code_maker = CodeMaker
-code_breaker = CodeBreaker
 
 # Code Maker (Computer) randomly selects 6 colors: red, green, blue, yellow, orange, purple 
 # Can be any color, any order, or any amount
-def computer_create_code(code_maker, color_pool)
+def computer_create_code(color_pool)
   colors = []
   4.times {colors.push(color_pool.sample)}
-  return code_maker.new(colors)
+  return CodeMaker.new(colors)
 end
 
-def play_game(code_maker)
+def play_game()
   color_pool = ['red','green','blue','yellow','orange','purple']
   # If COMPUTER is code maker
-  code_maker = computer_create_code(code_maker, color_pool)
+  code_maker = computer_create_code(color_pool)
   p code_maker
 end
 
-play_game(code_maker)
+play_game()
