13bd1787f0e8f69ba313f9062eb6560b45b2a0cd
Brady Barker <brady.d.barker@gmail.com>
2023-02-08 20:33:55 -0500

    Completed up to pop method for LinkedList class

--- bit_of_computer_science/linked_lists/lib/linked_list.rb
+++ bit_of_computer_science/linked_lists/lib/linked_list.rb
@@ -1,9 +1,61 @@
 require_relative "node.rb"
 
 class LinkedList
-  attr_accessor :head
+  attr_accessor :head_node
 
   def initialize()
-    @head = nil
+    @head_node = nil
+  end
+
+  def addFirst(value, next_node=nil)
+    self.head_node = Node.new(value, next_node)
+  end
+
+  def append(value)
+    if self.head_node == nil
+      addFirst(value)
+    else
+      current = self.head_node
+      current = current.next_node while current.next_node != nil 
+      current.next_node = Node.new(value, nil)
+    end
+  end
+
+  def prepend(value)
+    addFirst(value, self.head_node)
+  end
+
+  def size()
+    current = self.head_node
+    node_counter = 0
+    while current.next_node != nil do
+      node_counter += 1
+      current = current.next_node
+    end
+    return node_counter + 1
+  end
+
+  def head()
+    return self.head_node.value
+  end
+
+  def tail()
+    current = self.head_node
+    current = current.next_node while current.next_node != nil 
+    return current.value
+  end
+
+  def at(requested_index)
+    current = self.head_node
+    current_index = 0
+    while current_index < requested_index do
+      current = current.next_node
+      current_index += 1
+    end
+    return current.value
+  end
+
+  def pop()
+
   end
 end
--- bit_of_computer_science/linked_lists/main.rb
+++ bit_of_computer_science/linked_lists/main.rb
@@ -1,3 +1,15 @@
 require_relative "lib/linked_list.rb"
 
 list = LinkedList.new()
+
+list.append(5)
+list.append(4)
+list.append(3)
+list.prepend(10)
+list.prepend(12)
+
+p list
+p list.size
+p list.head
+p list.tail
+p list.at(3) 
