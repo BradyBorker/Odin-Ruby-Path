a19bdfe15bcb5b66be1ff6e041c64c60adb3ee13
Brady Barker <brady.d.barker@gmail.com>
2023-03-14 21:13:36 -0400

    Add third test: Works with small positive shift and big word

--- intro_to_RSpec/caesar_cipher_redone/spec/caesar_cipher_spec.rb
+++ intro_to_RSpec/caesar_cipher_redone/spec/caesar_cipher_spec.rb
@@ -11,5 +11,10 @@ describe Caesar_cipher do
       cipher = Caesar_cipher.new
       expect(cipher.translate('a', 50)).to eq('y')
     end
+
+    it "Works with small positive shift and big word" do
+      cipher = Caesar_cipher.new
+      expect(cipher.translate("What a string!", 5)).to eq('Bmfy f xywnsl!')
+    end
   end
 end
