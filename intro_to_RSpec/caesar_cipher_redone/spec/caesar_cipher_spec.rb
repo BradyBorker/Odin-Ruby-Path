ba6f066ee65a60bc24adb87e93eced5a0def6571
Brady Barker <brady.d.barker@gmail.com>
2023-03-14 21:11:22 -0400

    Create second test: 'Work with big positive shift and small word'

--- intro_to_RSpec/caesar_cipher_redone/spec/caesar_cipher_spec.rb
+++ intro_to_RSpec/caesar_cipher_redone/spec/caesar_cipher_spec.rb
@@ -3,8 +3,12 @@ require_relative '../lib/caesar_cipher'
 describe Caesar_cipher do
   describe "#translate" do
     it "Works with small positive shift and small word" do
-      cipher = Caesar_cipher.new()
+      cipher = Caesar_cipher.new
       expect(cipher.translate('a', 5)).to eq('f')
     end
+
+    it "Works with big positive shift and small word" do
+      cipher = Caesar_cipher.new
+      expect(cipher.translate('a', 50)).to eq('y')
   end
 end
