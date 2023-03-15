3bc7e81ca1ca4dc38ea8a8193816a74f2d647df2
Brady Barker <brady.d.barker@gmail.com>
2023-03-14 21:22:55 -0400

    Passed third test

--- intro_to_RSpec/caesar_cipher_redone/lib/caesar_cipher.rb
+++ intro_to_RSpec/caesar_cipher_redone/lib/caesar_cipher.rb
@@ -6,16 +6,18 @@ class Caesar_cipher
     remainder = shift % 26
 
     translated_indexes = indexes.map do |char_index|
-      next if !char_index.is_a? Numeric
-
-      remainder.times do 
-        char_index += 1
-        char_index = 97 if char_index > 122
+      if char_index.is_a?(Numeric)
+        remainder.times do 
+          char_index += 1
+          char_index = 97 if char_index > 122
+        end
+        char_index
+      else
+        char_index
       end
-      char_index
     end
 
-    return translated_indexes.map {|ord| ord.chr if ord.is_a?(Numeric)}.join
+    return translated_indexes.map {|item| item.is_a?(Numeric) ? item.chr : item}.join.capitalize
   end
 end
 
