e3a4807a4a2b7a529e0aed9afc8343e30fcbf2d0
Brady Barker <brady.d.barker@gmail.com>
2023-03-14 21:23:26 -0400

    Create fourth test

--- intro_to_RSpec/caesar_cipher_redone/spec/caesar_cipher_spec.rb
+++ intro_to_RSpec/caesar_cipher_redone/spec/caesar_cipher_spec.rb
@@ -4,17 +4,22 @@ describe Caesar_cipher do
   describe "#translate" do
     it "Works with small positive shift and small word" do
       cipher = Caesar_cipher.new
-      expect(cipher.translate('a', 5)).to eq('f')
+      expect(cipher.translate('Aa', 5)).to eq('Ff')
     end
 
     it "Works with big positive shift and small word" do
       cipher = Caesar_cipher.new
-      expect(cipher.translate('a', 50)).to eq('y')
+      expect(cipher.translate('Aa', 50)).to eq('Yy')
     end
 
     it "Works with small positive shift and big word" do
       cipher = Caesar_cipher.new
       expect(cipher.translate("What a string!", 5)).to eq('Bmfy f xywnsl!')
+    end
+
+    it "Works with big positive shift and big word" do
+      cipher = Caesar_cipher.new
+      expect(cipher.translate("Hello world", 50)).to eq("Fcjjm umpjb")
     end
   end
 end
