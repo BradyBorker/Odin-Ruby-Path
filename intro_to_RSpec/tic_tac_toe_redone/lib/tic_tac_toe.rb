6537fe8ed39796b2ba618532ba373cffc165819b
Brady Barker <brady.barker@id.me>
2023-03-27 12:36:04 -0400

    New tic-tac-toe game is functional. Beginning rspec testing

--- intro_to_RSpec/tic_tac_toe_redone/lib/tic_tac_toe.rb
+++ intro_to_RSpec/tic_tac_toe_redone/lib/tic_tac_toe.rb
@@ -11,23 +11,44 @@ class Board
         puts "#{player.name} Select a location between 1 and 9"
         location = gets.chomp().to_i
         until valid_move?(location)
-            puts "Invalid input, Needs to be between 1 and 9"
+            puts "Invalid input: Needs to be between 1 and 9 and empty"
             location = gets.chomp().to_i
         end
         location -= 1
 
         @board[location] = player.sign
+        remove_space()
+        print_board()
     end
 
     def valid_move?(input)
-        return true if input.between?(1, 9)
+        return true if input.between?(1, 9) && @board[input] != 'X' || @board[input] != 'O'
     end
 
     def remove_space()
         @spaces_remaining -= 1
     end
 
-    def game_over?
+    def game_over?(player)
+        @@win_states.each do |win_state|
+            count = 0
+            win_state.each do |coord|
+                if @board[coord] == player.sign
+                    count += 1
+                end
+
+                if count == 3
+                    puts "#{player.name} WINS!"
+                    return true
+                end
+            end
+        end
+
+        if @spaces_remaining == 0
+            puts "IT IS A TIE!"
+            return true
+        end
+        
         return false
     end
 
@@ -43,6 +64,9 @@ class Board
         print @board[6] + '|' + @board[7] + '|' + @board[8]
         puts ''
     end
+
+    private
+    attr_reader :win_states
 end
 
 class Player
@@ -61,25 +85,32 @@ class Player
     end
 end
 
+def switch_players(current_player, player1, player2)
+    if current_player == player1
+        current_player = player2
+    else
+        current_player = player1
+    end
+    return current_player
+end
+
 def play_game()
     player1 = Player.create_player
     player2 = Player.create_player
     board = Board.new
 
+    board.print_board
     current_player = player1
-    until board.game_over?
+    loop do
         # Announce player
         puts "Current Turn: #{current_player.name}"
         # Make move
         board.make_move(current_player)
-        # Check win
-
+        # Check if game over
+        return if board.game_over?(current_player)
         # Switch players
-        
-        # Print board
+        current_player = switch_players(current_player, player1, player2)
     end
 end
 
-board = Board.new()
-player = Player.new('john')
-board.make_move(player)
+play_game()
