7f34ae2b3e0be0f442485c79b2934d82c0141be1
Brady Barker <brady.d.barker@gmail.com>
2023-03-29 20:59:29 -0400

    Pawn class close to completion if not completed. Passes the following tests: #get_available_moves #prune_moves #out_of_bounds It also includes a #move method which calls #get_available_moves and #prune_moves

---
+++ chess_game/lib/pieces/pawn_class.rb
@@ -0,0 +1,52 @@
+class Pawn 
+  attr_accessor :position
+  attr_reader :piece, :color
+
+  def initialize(position, color)
+    @position = position
+    @color = color
+
+    color == 'white' ? @piece = "\u2659" : @piece = "\u265F"
+    color == 'white' ? @enemy = 'black' : @enemy = 'white'
+  end
+
+  def move(board_state)
+      moves = get_available_moves()
+      pruned_moves = prune_moves(board_state, moves)
+  end
+
+  def get_available_moves()
+    white_transformations = [[-1, 0], [-1, -1], [-1, 1]]
+    black_transformations = [[1, 0], [1, -1], [1, 1]]
+    
+    if @color == 'white'
+      moves = white_transformations.map {|t| [@position[0] + t[0], @position[1] + t[1]]}
+    elsif @color == 'black'
+      moves = black_transformations.map {|t| [@position[0] + t[0], @position[1] + t[1]]}
+    end
+
+    return moves
+  end
+
+  def prune_moves(board_state, moves)
+    pruned_moves = []
+    moves.each_with_index do |move, index|
+      next if out_of_bounds?(move)
+
+      if index == 0
+        pruned_moves.push(move) if board_state[move[0]][move[1]] == ' '
+      elsif index >= 1
+        next if board_state[move[0]][move[1]] == ' '
+
+        pruned_moves.push(move) if board_state[move[0]][move[1]].color == @enemy
+      end
+    end
+
+    pruned_moves
+  end
+
+  def out_of_bounds?(move)
+    return true if !(move[0].between?(0, 7)) || !(move[1].between?(0, 7))
+    false
+  end
+end
---
+++ chess_game/spec/pieces_spec.rb
@@ -0,0 +1,77 @@
+require_relative '../lib/pieces/pawn_class'
+
+describe Pawn do
+  describe '#get_available_moves' do
+    subject(:pawn) { described_class.new([1,1], 'white') }
+
+    it 'returns moves available for white pawn' do
+      expect(pawn.get_available_moves).to eq [[0,1], [0,0], [0,2]]
+    end
+
+    it 'returns moves available for black pawn' do
+      pawn.instance_variable_set(:@color, 'black')
+      expect(pawn.get_available_moves).to eq [[2,1], [2,0], [2,2]]
+    end
+  end
+
+  describe '#prune_moves' do
+    subject(:pawn) { described_class.new([1,1], 'white') }
+    let(:enemy) { double() }
+    let(:allie) { double() }
+
+    context 'Can move forward and has no enemies to attack' do
+      it 'returns one possible move' do
+        board_state = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        expect(pawn.prune_moves(board_state, [[0,1], [0,0], [0,2]])). to eq [[0,1]]
+      end
+    end
+
+    context 'Cannot move forward and has no enemies to attack' do
+      it 'returns an empty list' do
+        allow(enemy).to receive(:color).and_return('black')
+
+        board_state = [[' ', enemy, ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        expect(pawn.prune_moves(board_state, [[0,1], [0,0], [0,2]])). to eq []      
+      end
+    end
+
+    context 'Can move forward and has one enemy' do
+      it 'returns two possible moves' do
+        allow(enemy).to receive(:color).and_return('black')
+
+        board_state = [[enemy, ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        expect(pawn.prune_moves(board_state, [[0,1], [0,0], [0,2]])). to eq [[0,1], [0, 0]]     
+      end
+    end
+
+    context 'Allies are in attack area' do
+      it "won't attack allies only return forward movement" do
+        allow(enemy).to receive(:color).and_return('black')
+        allow(allie).to receive(:color).and_return('white')
+
+        board_state = [[allie, ' ', allie], [' ', ' ', ' '], [' ', ' ', ' ']]
+        expect(pawn.prune_moves(board_state, [[0,1], [0,0], [0,2]])). to eq [[0,1]]   
+      end
+    end
+  end
+
+  describe '#out_of_bounds?' do
+    subject(:pawn) { described_class.new([1,1], 'white') }
+
+    it 'returns true when row is neg out of bounds' do
+      expect(pawn.out_of_bounds?([-1,0])).to be true
+    end
+
+    it 'returns true when row is pos out of bounds' do
+      expect(pawn.out_of_bounds?([8,0])).to be true
+    end
+
+    it 'returns true when col is neg out of bounds' do
+      expect(pawn.out_of_bounds?([0,-1])).to be true
+    end
+
+    it 'returns true when col is pos out of bounds' do
+      expect(pawn.out_of_bounds?([0,8])).to be true
+    end
+  end
+end
