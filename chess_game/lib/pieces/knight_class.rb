c30bf8a16300773ea98ecf2c714eb097eb15cc7b
Brady Barker <brady.d.barker@gmail.com>
2023-03-30 18:38:07 -0400

    knight_class init

---
+++ chess_game/lib/pieces/knight_class.rb
@@ -0,0 +1,18 @@
+class Knight
+  attr_accessor :position
+  attr_reader :piece, :color
+
+  def initialize(position, color)
+    @position = position
+    @color = color
+
+    color == 'white' ? @piece = "\u2657" : @piece = "\u265D"
+    color == 'white' ? @enemy = 'black' : @enemy = 'white'
+  end
+
+  def get_valid_moves(board_state)
+    moves = get_possible_moves()
+    valid_moves = pruned_moves(board_state, moves)
+    return valid_moves
+  end
+end
